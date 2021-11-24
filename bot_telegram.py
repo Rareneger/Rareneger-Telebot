b3069e151ae3ffcffbdefa19c6a22136899de4c2
Tony ALbert Lima <80111558+TonyALima@users.noreply.github.com>
2021-11-24 20:28:07 -0300

    start telegram bot

--- bot_telegram.py
+++ bot_telegram.py
@@ -6,5 +6,17 @@ def get_api_key():
 
 bot = telebot.TeleBot(get_api_key())
 
+def is_true(msg):
+    return True
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.reply_to(message, 'oioi, tudo bem com você? aqui é do Rareneger')
+    default_message(message)
+
+@bot.message_handler(func=is_true)
+def default_message(message):
+    bot.reply_to(message, 'default message')
+
 if __name__ == '__main__':
     bot.polling()
